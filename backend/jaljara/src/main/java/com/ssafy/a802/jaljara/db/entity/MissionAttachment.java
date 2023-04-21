f2d16f0f73437cbdda40712ab6e348183d1c7e19
9jinseok <mer0120@naver.com>
2023-04-21 12:14:53 +0900

    [BE] Fix: OnDelete Cascade 수정

--- backend/jaljara/src/main/java/com/ssafy/a802/jaljara/db/entity/MissionAttachment.java
+++ backend/jaljara/src/main/java/com/ssafy/a802/jaljara/db/entity/MissionAttachment.java
@@ -1,10 +1,6 @@
 package com.ssafy.a802.jaljara.db.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class MissionAttachment {
@@ -14,6 +10,10 @@ public class MissionAttachment {
 	private Long id;
 
 	private String url;
+
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "mission_log_id")
+	private MissionLog missionLog;
 
 	@Enumerated(EnumType.STRING)
 	private MissionContents missionContents;
