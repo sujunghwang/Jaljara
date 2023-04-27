45a53923c33775d66289a9f51c1ade6f64a0b899
leehyeji <zzambbang@kakao.com>
2023-04-27 10:37:31 +0900

    [BE] Fix: 미션 투데이 셰듈 부분 수정

--- backend/jaljara/src/main/java/com/ssafy/a802/jaljara/JaljaraApplication.java
+++ backend/jaljara/src/main/java/com/ssafy/a802/jaljara/JaljaraApplication.java
@@ -3,9 +3,11 @@ package com.ssafy.a802.jaljara;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableScheduling
 public class JaljaraApplication {
 
 	public static void main(String[] args) {
--- backend/jaljara/src/main/java/com/ssafy/a802/jaljara/db/repository/UserRepository.java
+++ backend/jaljara/src/main/java/com/ssafy/a802/jaljara/db/repository/UserRepository.java
@@ -3,9 +3,12 @@ package com.ssafy.a802.jaljara.db.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ssafy.a802.jaljara.db.entity.User;
+import com.ssafy.a802.jaljara.db.entity.UserType;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findBySub(String sub);
+    List<User> findAllByUserType(UserType userType);
 }
