214b9e6b568b903fcd5dd2fd47408d3c6f041f39
jongseok-oh <hynb14@gmail.com>
2023-04-27 17:35:27 +0900

    [FE] Feat: 부모 페이지 라우팅 성공! ^^

---
+++ frontend/Jaljara/app/src/main/java/com/ssafy/jaljara/ui/screen/JaljaraScreen.kt
@@ -0,0 +1,9 @@
+package com.ssafy.jaljara.ui.screen
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun JaljaraApp(){
+
+}
+
---
+++ frontend/Jaljara/app/src/main/java/com/ssafy/jaljara/ui/screen/parent/SetTimeScreen.kt
@@ -0,0 +1,9 @@
+package com.ssafy.jaljara.ui.screen.parent
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SetTimeScreen(){
+    Text(text = "시간 설정 페이지")
+}
