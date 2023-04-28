e7af59171a640010427020f2be93056694cd8268
9jinseok <mer0120@naver.com>
2023-04-28 15:14:40 +0900

    [FE] Feat: 배경, 수면, 별자리 등 이미지 추가 / UI 색상, 크기 등 조정

--- frontend/Jaljara/app/src/main/java/com/ssafy/jaljara/ui/screen/child/StarLink.kt
+++ frontend/Jaljara/app/src/main/java/com/ssafy/jaljara/ui/screen/child/StarLink.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -27,11 +28,11 @@ fun StarLink(){
     var getBtnValid = false
     var rewardBtnValid = true
 
-    var streakCnt = 3
+    var streakCnt = 4
     if(streakCnt == 7)
         getBtnValid = true
 
-    var currentReward = "놀이동산 같이 가기~"
+    var currentReward = "놀이동산 같이 가기"
     if(currentReward == "")
         rewardBtnValid = false
 
@@ -39,58 +40,82 @@ fun StarLink(){
         mutableStateOf(false)
     }
 
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Top,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            Icon(painter = painterResource(id = R.drawable.ic_launcher_foreground), contentDescription = "icon")
-            Text(text = "바른 수면 별자리", fontSize = 40.sp)
-        }
-        Text(
-            text = "별자리 들어갈 곳!",
-            modifier = Modifier.padding(top = 150.dp, bottom = 150.dp)
+    Box(modifier = Modifier.fillMaxSize()){
+        Image(
+            painter = painterResource(R.drawable.bg),
+            contentDescription = "background",
+            contentScale = ContentScale.FillBounds,
+            modifier = Modifier.fillMaxSize()
         )
-        Text(
-            text = "${streakCnt} / 7",
-            fontSize = 24.sp,
-            modifier = Modifier.padding(top = 16.dp, bottom = 16.dp)
-        )
-        Button(
-            onClick = {
-                      openDialog.value = true
-            },
-            contentPadding = PaddingValues(12.dp),
-            colors = ButtonDefaults.buttonColors(Color(red = 210, green = 70, blue = 210)),
-            modifier = Modifier
-                .wrapContentSize()
-                .padding(top = 16.dp, bottom = 16.dp),
-            enabled = rewardBtnValid
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally,
         ) {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Image(painter = painterResource(id = R.drawable.astronoutsleep), contentDescription = "icon")
+                Text(
+                    text = "바른 수면 별자리",
+                    color = Color.White,
+                    fontSize = 40.sp
+                )
+            }
             Image(
-                painter = painterResource(id = R.drawable.baseline_card_giftcard_24),
-                contentDescription = "present drawable",
-                modifier = Modifier.size(36.dp)
+                painter = painterResource(id = R.drawable.star_link),
+                contentDescription = "star_link",
+                modifier = Modifier.size(size = 360.dp).padding(16.dp)
             )
+            Box(
+                modifier = Modifier
+                    .wrapContentSize()
+                    .padding(16.dp)
+                    .background(color = Color(0x40BAD6F0), shape = RoundedCornerShape(16.dp))
+            ){
+                Text(
+                    text = "${streakCnt} / 7",
+                    color = Color.White,
+                    fontSize = 24.sp,
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+            Button(
+                onClick = {
+                    openDialog.value = true
+                },
+                contentPadding = PaddingValues(12.dp),
+                colors = ButtonDefaults.buttonColors(Color(0xFF674AB3)),
+                modifier = Modifier
+                    .wrapContentSize()
+                    .padding(top = 16.dp, bottom = 16.dp),
+                enabled = rewardBtnValid
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.baseline_card_giftcard_24),
+                    contentDescription = "present drawable",
+                    modifier = Modifier.size(36.dp)
+                )
+            }
+            Button(
+                onClick = {},
+                contentPadding = PaddingValues(12.dp),
+                colors = ButtonDefaults.buttonColors(
+                    disabledContainerColor = Color.LightGray,
+                    disabledContentColor = Color.Gray,
+                    containerColor = Color.Blue,
+                    contentColor = Color.White),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp),
+                enabled = getBtnValid
+            ) {
+                Text(text = "보상 획득")
+            }
         }
-        Button(
-            onClick = {},
-            contentPadding = PaddingValues(12.dp),
-            colors = ButtonDefaults.buttonColors(Color.Blue),
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(10.dp),
-            enabled = getBtnValid
-        ) {
-            Text(text = "보상 획득")
+
+        if(openDialog.value){
+            RewardDialog(currentReward) { openDialog.value = false }
         }
     }
-
-    if(openDialog.value){
-        RewardDialog(currentReward) { openDialog.value = false }
-    }
-
 }
 
 @Composable
@@ -109,7 +134,7 @@ fun RewardDialog(reward: String, onDismiss: () -> Unit) {
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(150.dp)
-                    .background(color = Color(red = 210, green = 70, blue = 210)),
+                    .background(color = Color(0xFF674AB3)),
                 verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.Center
             ) {
