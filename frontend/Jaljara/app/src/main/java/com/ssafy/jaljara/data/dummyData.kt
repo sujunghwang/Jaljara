30117d2bb0be82b20606b32354fe3670c365f3b9
choijiyeon <choiwldus@naver.com>
2023-05-01 16:28:32 +0900

    feat: 아이메인페이지 디자인 초기 구현

--- frontend/Jaljara/app/src/main/java/com/ssafy/jaljara/data/dummyData.kt
+++ frontend/Jaljara/app/src/main/java/com/ssafy/jaljara/data/dummyData.kt
@@ -4,3 +4,11 @@ object DummyDataProvider {
 
     val contentList = List<Content>(200){ Content() }
 }
+
+data class DummyChildSleepInfo (
+    val childId: Long=1,
+    val currentReward: String="이닦는 사진 찍기",
+    val streakCount: Int=1,
+    val targetBedTime: String="22:00:00",
+    val targetWakeupTime: String="08:00:00"
+)
