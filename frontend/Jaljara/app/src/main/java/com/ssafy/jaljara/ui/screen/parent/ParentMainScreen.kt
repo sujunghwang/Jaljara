5ca229732ba5ef44e1787c10b837f1f45da35fec
sujung <hsj8518@gmail.com>
2023-05-08 09:08:36 +0900

    [Fe] Feat: GIF 넣는 코드 추가

--- frontend/Jaljara/app/src/main/java/com/ssafy/jaljara/ui/screen/parent/ParentMainScreen.kt
+++ frontend/Jaljara/app/src/main/java/com/ssafy/jaljara/ui/screen/parent/ParentMainScreen.kt
@@ -35,6 +35,8 @@ import coil.compose.rememberAsyncImagePainter
 import coil.decode.GifDecoder
 import coil.decode.ImageDecoderDecoder
 import com.bumptech.glide.Glide
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
 import com.bumptech.glide.request.target.CustomTarget
 import com.bumptech.glide.request.transition.Transition
 import com.ssafy.jaljara.R
@@ -54,15 +56,6 @@ fun ParentMainView(parentViewModel: ParentViewModel){
     var childSleepInfo = parentViewModel.childSleepResponse
     var todayMission = parentViewModel.todayMissionResponse
 
-//    val imageLoader = ImageLoader.Builder(LocalContext.current)
-//        .components {
-//            if (SDK_INT >= 28) {
-//                add(ImageDecoderDecoder.Factory())
-//            } else {
-//                add(GifDecoder.Factory())
-//            }
-//        }
-//        .build()
     Box(modifier = Modifier.fillMaxSize()){
         Image(
             painter = painterResource(R.drawable.bg),
@@ -80,8 +73,10 @@ fun ParentMainView(parentViewModel: ParentViewModel){
             parentViewModel.getTodayMission(1)
             parentViewModel.getChildSleepInfo(1)
             Children(DummyDataProvider.childList)
-            CurrentRewardContainer(painterResource(R.drawable.current_reward),"현재 보상", childSleepInfo.currentReward)
-            CurrentRewardContainer(painterResource(id = R.drawable.today_mission),"오늘의 미션", todayMission.content)
+//            CurrentRewardContainer(painterResource(R.drawable.current_reward),"현재 보상", childSleepInfo.currentReward)
+//            CurrentRewardContainer(painterResource(id = R.drawable.today_mission),"오늘의 미션", todayMission.content)
+            CurrentRewardContainer(R.drawable.current_reward_2,"현재 보상", childSleepInfo.currentReward)
+            CurrentRewardContainer(R.drawable.reward_2,"오늘의 미션", todayMission.content)
             Row(modifier = Modifier.fillMaxWidth()) {
                 ChildSetTimeCard(painterResource(id = R.drawable.baseline_alarm_24),"Wake Up",
                     "${if (childSleepInfo.targetWakeupTime!="") childSleepInfo.targetWakeupTime.substring(0, 5) else childSleepInfo.targetWakeupTime}", Modifier.weight(1f))
@@ -223,9 +218,9 @@ fun Child(childInfo: ChildInfo) {
 }
 
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalGlideComposeApi::class)
 @Composable
-fun CurrentRewardContainer(img : Painter, title:String, content: String) {
+fun CurrentRewardContainer(img : Int, title:String, content: String) {
     Card(
         modifier = Modifier
             .fillMaxWidth(),
@@ -240,11 +235,15 @@ fun CurrentRewardContainer(img : Painter, title:String, content: String) {
             Row(
                 verticalAlignment = Alignment.CenterVertically,
             ) {
-                Image(painter = img,
+                GlideImage(
+                    model = img,
                     contentDescription = null,
-                    modifier = Modifier.size(110.dp, 110.dp)
-                        .padding(15.dp)
                 )
+//                Image(painter = img,
+//                    contentDescription = null,
+//                    modifier = Modifier.size(110.dp, 110.dp)
+//                        .padding(15.dp)
+//                )
                 Column() {
                     Text(text = title, color = Color.White)
                     Text(text = "$content", fontSize = 20.sp, color = Color.White)
@@ -286,46 +285,10 @@ fun ChildSetTimeCard(img : Painter,title:String, content: String, modifier: Modi
     )
 }
 
-@Composable
-fun GifComponent() {
-    val imageLoader = ImageLoader.Builder(LocalContext.current)
-        .components {
-            if (SDK_INT >= 28) {
-                add(ImageDecoderDecoder.Factory())
-            } else {
-                add(GifDecoder.Factory())
-            }
-        }
-        .build()
-
-    Image(
-        painter = rememberAsyncImagePainter(R.drawable.reward_2, imageLoader),
-        contentDescription = null,
-//        modifier = Modifier.fillMaxSize()
-    )
-}
-
-//@Preview
-//@Composable
-//fun PreviewGifComponent() {
-//    GifComponent()
-//}
-
-
-
 @Preview(showBackground = true)
 @Composable
 fun ParentMainScreenView() {
     val scrollState = rememberScrollState()
-//    val imageLoader = ImageLoader.Builder(LocalContext.current)
-//        .components {
-//            if (SDK_INT >= 28) {
-//                add(ImageDecoderDecoder.Factory())
-//            } else {
-//                add(GifDecoder.Factory())
-//            }
-//        }
-//        .build()
 
     Box(modifier = Modifier.fillMaxSize()){
         Image(
@@ -342,8 +305,10 @@ fun ParentMainScreenView() {
             verticalArrangement = Arrangement.SpaceBetween
         ) {
             Children(DummyDataProvider.childList)
-            CurrentRewardContainer(painterResource(R.drawable.current_reward),"현재 보상", "놀이동산 가기")
-            CurrentRewardContainer(painterResource(id = R.drawable.today_mission),"오늘의 미션", "이닦는 사진 찍기")
+//            CurrentRewardContainer(painterResource(R.drawable.current_reward),"현재 보상", "놀이동산 가기")
+//            CurrentRewardContainer(painterResource(id = R.drawable.today_mission),"오늘의 미션", "이닦는 사진 찍기")
+            CurrentRewardContainer(R.drawable.current_reward_2,"현재 보상", "놀이동산 가기")
+            CurrentRewardContainer(R.drawable.reward_2,"오늘의 미션", "이닦는 사진 찍기")
             Row(modifier = Modifier.fillMaxWidth()) {
                 ChildSetTimeCard(painterResource(id = R.drawable.baseline_alarm_24),"Wake Up", "5:00", Modifier.weight(1f))
                 Spacer(modifier = Modifier.weight(0.1f))
