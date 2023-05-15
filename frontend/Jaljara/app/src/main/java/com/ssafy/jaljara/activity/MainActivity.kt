7297faf5a7e5af80f05831af32a29723a2e2975e
sujung <hsj8518@gmail.com>
2023-05-15 13:02:07 +0900

    [FE] Feat: 알람 클릭 시 알람 삭제 구현

--- frontend/Jaljara/app/src/main/java/com/ssafy/jaljara/activity/MainActivity.kt
+++ frontend/Jaljara/app/src/main/java/com/ssafy/jaljara/activity/MainActivity.kt
@@ -40,8 +40,8 @@ class MainActivity : ComponentActivity() {
         )
         setContent {
             JaljaraTheme {
-//                ChildApp(childViewModel)
-                ParentApp(parentViewModel)
+                ChildApp(childViewModel)
+//                ParentApp(parentViewModel)
 //                LandingApp(viewModel = landingViewModel)
             }
         }
