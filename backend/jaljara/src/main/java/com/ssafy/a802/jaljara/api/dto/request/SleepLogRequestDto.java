124be0e14a390587baf16bda7a682670df4317df
jongseok-oh <hynb14@gmail.com>
2023-05-15 17:28:47 +0900

    [BE] Feat: 수명 로그 저장 로직 수정

--- backend/jaljara/src/main/java/com/ssafy/a802/jaljara/api/dto/request/SleepLogRequestDto.java
+++ backend/jaljara/src/main/java/com/ssafy/a802/jaljara/api/dto/request/SleepLogRequestDto.java
@@ -13,10 +13,15 @@ public class SleepLogRequestDto {
     @AllArgsConstructor
     @NoArgsConstructor
     @Builder
-    public static class SleepLogInput {
-        private long userId;
-        private Date date;
-        private Time bedTime;
-        private Time wakeupTime;
+    public static class SleepSegmentEvent {
+        public static final int STATUS_SUCCESSFUL = 0;
+        public static final int STATUS_MISSING_DATA = 1;
+        public static final int STATUS_NOT_DETECTED = 2;
+
+        private Long userId;
+        private Long startTimeMillis;
+        private Long endTimeMills;
+        private Long segmentDurationMillis;
+        private Integer status;
     }
 }
