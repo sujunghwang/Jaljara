eb58b89a849b6a5e333ba732a246c047c1d94beb
choijiyeon <choiwldus@naver.com>
2023-05-16 10:01:59 +0900

    [FE] Fix: 수면설정시간 표시 변경

--- frontend/Jaljara/app/src/main/java/com/ssafy/jaljara/ui/screen/parent/ParentMainScreen.kt
+++ frontend/Jaljara/app/src/main/java/com/ssafy/jaljara/ui/screen/parent/ParentMainScreen.kt
@@ -33,6 +33,7 @@ import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -412,7 +413,7 @@ fun calTime(childSleepInfo: ChildSleepInfo): String {
 
     var sleepTime = LocalTime.of((wakeupTimeInt - bedTimeInt) / 60, (wakeupTimeInt - bedTimeInt) % 60)
 
-    var text = "${sleepTime.hour}H"
+    var text = "${sleepTime.hour}H ${sleepTime.minute}M"
 
     return text
 }
